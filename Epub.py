efdd01cf722c6f1a12cf4a425e1f125f4aad014a
Ruiwen Chua <ruiwen+github@thoughtmonkeys.com>
2010-06-28 14:32:17 +0800

    Refactoring BadEpubException (and other future Exceptions) out into its (their)  own file

--- Epub.py
+++ Epub.py
@@ -113,16 +113,6 @@ class EPub:
 		return self.filename
 
 
-class BadEpubException(Exception):
-	pass
 
-#	# Copied from http://stackoverflow.com/questions/1319615/proper-way-to-declare-custom-exceptions-in-modern-python
-#    def __init__(self, message, Errors):
-#	
-#	    # Call the base class constructor with the parameters it needs
-#	    Exception.__init__(self, message)
-#	
-#	    # Now for your custom code...
-#	    self.Errors = Errors
 			
 		
